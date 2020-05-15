d22349c2cb009d391c7f3f88a96627415d9639e6
gabriel.bur@outlook.com <gabriel.bur@outlook.com>
2020-05-14 23:13:00 -0300

    adding CrossCutting for IOC

---
+++ src/back/Infra.CrossCutting.IOC/ConfigurationIOC.cs
@@ -0,0 +1,24 @@
+﻿using Application.Services;
+using Autofac;
+using Domain.Core.Interfaces.Repository;
+using Domain.Core.Interfaces.Service;
+using Infra.Data.Repository.Repositories;
+
+namespace Infra.CrossCutting.IOC
+{
+    public class ConfigurationIOC
+    {
+        public static void LoadConfiguration(ContainerBuilder builder)
+        {
+            #region Repository
+            builder.RegisterType<UserRepository>().As<IUserRepository>();
+            builder.RegisterType<ErrorRepository>().As<IErrorRepository>();
+            #endregion
+
+
+            #region Services
+            builder.RegisterType<UserService>().As<IUserService>();
+            #endregion
+        }
+    }
+}
