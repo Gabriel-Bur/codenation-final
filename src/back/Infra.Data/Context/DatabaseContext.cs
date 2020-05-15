c55b8b36237b193dc3da243c4b50e68d737a8211
gabriel.bur@outlook.com <gabriel.bur@outlook.com>
2020-05-14 23:10:53 -0300

    added Data Context + EF Core

---
+++ src/back/Infra.Data/Context/DatabaseContext.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Core;
+using Domain.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Infra.Data.Context
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Error> Errors { get; set; }
+
+        public DatabaseContext()
+        {
+
+        }
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<BaseEntity>().HasQueryFilter(e => e.DeletionDateTime.Equals(null));
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
