cbecff70e6f89694947e17ef5c02226592ea379c
gabriel.bur@outlook.com <gabriel.bur@outlook.com>
2020-06-13 00:17:48 -0300

    changes in main files

---
+++ src/Api/AppSecret.cs
@@ -0,0 +1,10 @@
+﻿namespace Api
+{
+    public class AppSecret
+    {
+        public string Key { get; set; }
+        public int Expiration { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
---
+++ src/Api/Startup.cs
@@ -0,0 +1,64 @@
+using Api.Configuration;
+using Api.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDatabaseSetup(Configuration);
+
+            services.AddAutoMapperSetup();
+
+            services.AddIdentitySetup(Configuration);
+
+            services.AddSwaggerSetup();
+
+            services.AddDependencyInjectionSetup();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseSwaggerSetup();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+            app.UseMiddleware<CustomResponseMiddleware>();
+        }
+    }
+}
